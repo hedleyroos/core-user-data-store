eec4d4441d338a40f7d41c22e06ac587915cf8cd
ruands <ruan.desousa@praekeltconsulting.com>
2017-12-04 21:06:08 +0200

    GE-607: Travis and Tox config

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="core-user-data-store",
+    version="0.0.1",
+    description="Girl Effect Core User Data Store",
+    long_description=open("README.rst", "r").read() + open("AUTHORS.rst", "r").read() + open("CHANGELOG.rst", "r").read(),
+    author="Praekelt Consulting",
+    author_email="dev@praekelt.com",
+    license="BSD",
+    url="http://github.com/girleffect/core-user-data-store",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
