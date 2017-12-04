31f7919d8cab96e6445ba49649276a6facc34e60
AltusBarry <AltusBarry@users.noreply.github.com>
2017-12-04 15:51:52 +0200

    develop: Added docs

---
+++ docs/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:52,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to Girl Effect: Core user data store\u2019s documentation!"],titleterms:{core:0,data:0,document:0,effect:0,girl:0,indic:0,store:0,tabl:0,user:0,welcom:0}})
