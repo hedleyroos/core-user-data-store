0ffcdfa182dc7aee7a0364b5806941296425f2ce
ruands <ruan.desousa@praekeltconsulting.com>
2018-01-18 08:51:08 +0200

    GE-710: More models for UDS

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.postgresql import UUID
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = \
@@ -11,3 +12,21 @@ class SiteDataSchema(db.Model):
     __tablename__ = "sitedataschema"
     site_id = db.Column(db.Integer, primary_key=True)
     schema = db.Column(db.JSON)
+
+
+class UserSiteData(db.Model):
+    __tablename__ = "usersitedata"
+    user_id = db.Column(UUID, primary_key=True)
+    site_id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.JSON)
+    consented_at = db.Column(db.DateTime)
+    blocked = db.Column(db.Boolean)
+
+
+class AdminNote(db.Model):
+    __tablename__ = "adminnote"
+    user_id = db.Column(UUID, primary_key=True)
+    creator_id = db.Column(UUID, primary_key=True)
+    created_at = db.Column(db.DateTime, primary_key=True)
+    note = db.Column(db.Text)
+    updated_at = db.Column(db.DateTime)
