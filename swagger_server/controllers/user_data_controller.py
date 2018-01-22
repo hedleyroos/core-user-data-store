8f6e4d410776a869b3420b65049987c4dd7545f3
cobusc <cobus.carstens@gmail.com>
2018-01-22 10:23:12 +0200

    Added code generated by the new (2.3.1) version of Swagger codegen

--- swagger_server/controllers/user_data_controller.py
+++ swagger_server/controllers/user_data_controller.py
@@ -1,92 +1,108 @@
 import connexion
-from swagger_server.models.admin_note import AdminNote
-from swagger_server.models.admin_note_update import AdminNoteUpdate
-from swagger_server.models.country import Country
-from swagger_server.models.country_update import CountryUpdate
-from swagger_server.models.site_data_schema import SiteDataSchema
-from swagger_server.models.site_data_schema_update import SiteDataSchemaUpdate
-from swagger_server.models.user_site_data import UserSiteData
-from swagger_server.models.user_site_data_update import UserSiteDataUpdate
-from datetime import date, datetime
-from typing import List, Dict
-from six import iteritems
-from ..util import deserialize_date, deserialize_datetime
+import six
+
+from swagger_server.models.admin_note import AdminNote  # noqa: E501
+from swagger_server.models.admin_note_update import AdminNoteUpdate  # noqa: E501
+from swagger_server.models.country import Country  # noqa: E501
+from swagger_server.models.country_update import CountryUpdate  # noqa: E501
+from swagger_server.models.site_data_schema import SiteDataSchema  # noqa: E501
+from swagger_server.models.site_data_schema_update import SiteDataSchemaUpdate  # noqa: E501
+from swagger_server.models.user_site_data import UserSiteData  # noqa: E501
+from swagger_server.models.user_site_data_update import UserSiteDataUpdate  # noqa: E501
+from swagger_server import util
 
 
-def adminnote_create(data=None):
-    """
-    adminnote_create
-    
+def adminnote_create(data=None):  # noqa: E501
+    """adminnote_create
+
+     # noqa: E501
+
     :param data: 
     :type data: dict | bytes
 
     :rtype: AdminNote
     """
     if connexion.request.is_json:
-        data = AdminNote.from_dict(connexion.request.get_json())
+        data = AdminNote.from_dict(connexion.request.get_json())  # noqa: E501
     return 'do some magic!'
 
 
-def adminnote_delete(user_id, creator_id, created_at):
-    """
-    adminnote_delete
-    
+def adminnote_delete(user_id, creator_id, created_at):  # noqa: E501
+    """adminnote_delete
+
+     # noqa: E501
+
     :param user_id: A UUID value identifying the user.
-    :type user_id: str
+    :type user_id: dict | bytes
     :param creator_id: The creator_id
-    :type creator_id: str
+    :type creator_id: dict | bytes
     :param created_at: The created_at value
     :type created_at: str
 
     :rtype: None
     """
-    created_at = deserialize_datetime(created_at)
+    if connexion.request.is_json:
+        user_id = .from_dict(connexion.request.get_json())  # noqa: E501
+    if connexion.request.is_json:
+        creator_id = .from_dict(connexion.request.get_json())  # noqa: E501
+    created_at = util.deserialize_datetime(created_at)
     return 'do some magic!'
 
 
-def adminnote_list(offset=None, limit=None, user_id=None, creator_id=None):
-    """
-    adminnote_list
-    
+def adminnote_list(offset=None, limit=None, user_id=None, creator_id=None):  # noqa: E501
+    """adminnote_list
+
+     # noqa: E501
+
     :param offset: An optional query parameter specifying the offset in the result set to start from.
     :type offset: int
     :param limit: An optional query parameter to limit the number of results returned.
     :type limit: int
     :param user_id: An optional query parameter to filter by user_id
-    :type user_id: str
+    :type user_id: dict | bytes
     :param creator_id: An optional query parameter to filter by creator (a user_id)
-    :type creator_id: str
+    :type creator_id: dict | bytes
 
     :rtype: List[AdminNote]
     """
+    if connexion.request.is_json:
+        user_id = .from_dict(connexion.request.get_json())  # noqa: E501
+    if connexion.request.is_json:
+        creator_id = .from_dict(connexion.request.get_json())  # noqa: E501
     return 'do some magic!'
 
 
-def adminnote_read(user_id, creator_id, created_at):
-    """
-    adminnote_read
-    
+def adminnote_read(user_id, creator_id, created_at):  # noqa: E501
+    """adminnote_read
+
+     # noqa: E501
+
     :param user_id: A UUID value identifying the user.
-    :type user_id: str
+    :type user_id: dict | bytes
     :param creator_id: The creator_id
-    :type creator_id: str
+    :type creator_id: dict | bytes
     :param created_at: The created_at value
     :type created_at: str
 
     :rtype: AdminNote
     """
-    created_at = deserialize_datetime(created_at)
+    if connexion.request.is_json:
+        user_id = .from_dict(connexion.request.get_json())  # noqa: E501
+    if connexion.request.is_json:
+        creator_id = .from_dict(connexion.request.get_json())  # noqa: E501
+    created_at = util.deserialize_datetime(created_at)
     return 'do some magic!'
 
 
-def adminnote_update(user_id, creator_id, created_at, data=None):
-    """
-    adminnote_update
-    
+def adminnote_update(user_id, creator_id, created_at, data=None):  # noqa: E501
+    """adminnote_update
+
+     # noqa: E501
+
     :param user_id: A UUID value identifying the user.
-    :type user_id: str
+    :type user_id: dict | bytes
     :param creator_id: The creator_id
-    :type creator_id: str
+    :type creator_id: dict | bytes
     :param created_at: The created_at value
     :type created_at: str
     :param data: 
@@ -94,30 +110,36 @@ def adminnote_update(user_id, creator_id, created_at, data=None):
 
     :rtype: AdminNote
     """
-    created_at = deserialize_datetime(created_at)
     if connexion.request.is_json:
-        data = AdminNoteUpdate.from_dict(connexion.request.get_json())
+        user_id = .from_dict(connexion.request.get_json())  # noqa: E501
+    if connexion.request.is_json:
+        creator_id = .from_dict(connexion.request.get_json())  # noqa: E501
+    created_at = util.deserialize_datetime(created_at)
+    if connexion.request.is_json:
+        data = AdminNoteUpdate.from_dict(connexion.request.get_json())  # noqa: E501
     return 'do some magic!'
 
 
-def country_create(data=None):
-    """
-    country_create
-    
+def country_create(data=None):  # noqa: E501
+    """country_create
+
+     # noqa: E501
+
     :param data: 
     :type data: dict | bytes
 
     :rtype: Country
     """
     if connexion.request.is_json:
-        data = Country.from_dict(connexion.request.get_json())
+        data = Country.from_dict(connexion.request.get_json())  # noqa: E501
     return 'do some magic!'
 
 
-def country_delete(country_code):
-    """
-    country_delete
-    
+def country_delete(country_code):  # noqa: E501
+    """country_delete
+
+     # noqa: E501
+
     :param country_code: A unique two-character value identifying the country.
     :type country_code: str
 
@@ -126,10 +148,11 @@ def country_delete(country_code):
     return 'do some magic!'
 
 
-def country_list(limit=None, offset=None):
-    """
-    country_list
-    
+def country_list(limit=None, offset=None):  # noqa: E501
+    """country_list
+
+     # noqa: E501
+
     :param limit: An optional query parameter to limit the number of results returned.
     :type limit: int
     :param offset: An optional query parameter specifying the offset in the result set to start from.
@@ -140,10 +163,11 @@ def country_list(limit=None, offset=None):
     return 'do some magic!'
 
 
-def country_read(country_code):
-    """
-    country_read
-    
+def country_read(country_code):  # noqa: E501
+    """country_read
+
+     # noqa: E501
+
     :param country_code: A unique two-character value identifying the country.
     :type country_code: str
 
@@ -152,10 +176,11 @@ def country_read(country_code):
     return 'do some magic!'
 
 
-def country_update(country_code, data=None):
-    """
-    country_update
-    
+def country_update(country_code, data=None):  # noqa: E501
+    """country_update
+
+     # noqa: E501
+
     :param country_code: A unique two-character value identifying the country.
     :type country_code: str
     :param data: 
@@ -164,28 +189,30 @@ def country_update(country_code, data=None):
     :rtype: Country
     """
     if connexion.request.is_json:
-        data = CountryUpdate.from_dict(connexion.request.get_json())
+        data = CountryUpdate.from_dict(connexion.request.get_json())  # noqa: E501
     return 'do some magic!'
 
 
-def sitedataschema_create(data=None):
-    """
-    sitedataschema_create
-    
+def sitedataschema_create(data=None):  # noqa: E501
+    """sitedataschema_create
+
+     # noqa: E501
+
     :param data: 
     :type data: dict | bytes
 
     :rtype: SiteDataSchema
     """
     if connexion.request.is_json:
-        data = SiteDataSchema.from_dict(connexion.request.get_json())
+        data = SiteDataSchema.from_dict(connexion.request.get_json())  # noqa: E501
     return 'do some magic!'
 
 
-def sitedataschema_delete(site_id):
-    """
-    sitedataschema_delete
-    
+def sitedataschema_delete(site_id):  # noqa: E501
+    """sitedataschema_delete
+
+     # noqa: E501
+
     :param site_id: A unique integer value identifying the site.
     :type site_id: int
 
@@ -194,10 +221,11 @@ def sitedataschema_delete(site_id):
     return 'do some magic!'
 
 
-def sitedataschema_list(offset=None, limit=None):
-    """
-    sitedataschema_list
-    
+def sitedataschema_list(offset=None, limit=None):  # noqa: E501
+    """sitedataschema_list
+
+     # noqa: E501
+
     :param offset: An optional query parameter specifying the offset in the result set to start from.
     :type offset: int
     :param limit: An optional query parameter to limit the number of results returned.
@@ -208,10 +236,11 @@ def sitedataschema_list(offset=None, limit=None):
     return 'do some magic!'
 
 
-def sitedataschema_read(site_id):
-    """
-    sitedataschema_read
-    
+def sitedataschema_read(site_id):  # noqa: E501
+    """sitedataschema_read
+
+     # noqa: E501
+
     :param site_id: A unique integer value identifying the site.
     :type site_id: int
 
@@ -220,10 +249,11 @@ def sitedataschema_read(site_id):
     return 'do some magic!'
 
 
-def sitedataschema_update(site_id, data=None):
-    """
-    sitedataschema_update
-    
+def sitedataschema_update(site_id, data=None):  # noqa: E501
+    """sitedataschema_update
+
+     # noqa: E501
+
     :param site_id: A unique integer value identifying the site.
     :type site_id: int
     :param data: 
@@ -232,76 +262,87 @@ def sitedataschema_update(site_id, data=None):
     :rtype: SiteDataSchema
     """
     if connexion.request.is_json:
-        data = SiteDataSchemaUpdate.from_dict(connexion.request.get_json())
+        data = SiteDataSchemaUpdate.from_dict(connexion.request.get_json())  # noqa: E501
     return 'do some magic!'
 
 
-def usersitedata_create(data=None):
-    """
-    usersitedata_create
-    
+def usersitedata_create(data=None):  # noqa: E501
+    """usersitedata_create
+
+     # noqa: E501
+
     :param data: 
     :type data: dict | bytes
 
     :rtype: UserSiteData
     """
     if connexion.request.is_json:
-        data = UserSiteData.from_dict(connexion.request.get_json())
+        data = UserSiteData.from_dict(connexion.request.get_json())  # noqa: E501
     return 'do some magic!'
 
 
-def usersitedata_delete(user_id, site_id):
-    """
-    usersitedata_delete
-    
+def usersitedata_delete(user_id, site_id):  # noqa: E501
+    """usersitedata_delete
+
+     # noqa: E501
+
     :param user_id: A UUID value identifying the user.
-    :type user_id: str
+    :type user_id: dict | bytes
     :param site_id: A unique integer value identifying the site.
     :type site_id: int
 
     :rtype: None
     """
+    if connexion.request.is_json:
+        user_id = .from_dict(connexion.request.get_json())  # noqa: E501
     return 'do some magic!'
 
 
-def usersitedata_list(offset=None, limit=None, user_id=None, site_id=None):
-    """
-    usersitedata_list
-    
+def usersitedata_list(offset=None, limit=None, user_id=None, site_id=None):  # noqa: E501
+    """usersitedata_list
+
+     # noqa: E501
+
     :param offset: An optional query parameter specifying the offset in the result set to start from.
     :type offset: int
     :param limit: An optional query parameter to limit the number of results returned.
     :type limit: int
     :param user_id: An optional query parameter to filter by user_id
-    :type user_id: str
+    :type user_id: dict | bytes
     :param site_id: An optional query parameter to filter by site_id
     :type site_id: int
 
     :rtype: List[UserSiteData]
     """
+    if connexion.request.is_json:
+        user_id = .from_dict(connexion.request.get_json())  # noqa: E501
     return 'do some magic!'
 
 
-def usersitedata_read(user_id, site_id):
-    """
-    usersitedata_read
-    
+def usersitedata_read(user_id, site_id):  # noqa: E501
+    """usersitedata_read
+
+     # noqa: E501
+
     :param user_id: A UUID value identifying the user.
-    :type user_id: str
+    :type user_id: dict | bytes
     :param site_id: A unique integer value identifying the site.
     :type site_id: int
 
     :rtype: UserSiteData
     """
+    if connexion.request.is_json:
+        user_id = .from_dict(connexion.request.get_json())  # noqa: E501
     return 'do some magic!'
 
 
-def usersitedata_update(user_id, site_id, data=None):
-    """
-    usersitedata_update
-    
+def usersitedata_update(user_id, site_id, data=None):  # noqa: E501
+    """usersitedata_update
+
+     # noqa: E501
+
     :param user_id: A UUID value identifying the user.
-    :type user_id: str
+    :type user_id: dict | bytes
     :param site_id: A unique integer value identifying the site.
     :type site_id: int
     :param data: 
@@ -310,5 +351,7 @@ def usersitedata_update(user_id, site_id, data=None):
     :rtype: UserSiteData
     """
     if connexion.request.is_json:
-        data = UserSiteDataUpdate.from_dict(connexion.request.get_json())
+        user_id = .from_dict(connexion.request.get_json())  # noqa: E501
+    if connexion.request.is_json:
+        data = UserSiteDataUpdate.from_dict(connexion.request.get_json())  # noqa: E501
     return 'do some magic!'
--- swagger_server/models/__init__.py
+++ swagger_server/models/__init__.py
@@ -1,12 +1,13 @@
 # coding: utf-8
 
+# flake8: noqa
 from __future__ import absolute_import
 # import models into model package
-from .admin_note import AdminNote
-from .admin_note_update import AdminNoteUpdate
-from .country import Country
-from .country_update import CountryUpdate
-from .site_data_schema import SiteDataSchema
-from .site_data_schema_update import SiteDataSchemaUpdate
-from .user_site_data import UserSiteData
-from .user_site_data_update import UserSiteDataUpdate
+from swagger_server.models.admin_note import AdminNote
+from swagger_server.models.admin_note_update import AdminNoteUpdate
+from swagger_server.models.country import Country
+from swagger_server.models.country_update import CountryUpdate
+from swagger_server.models.site_data_schema import SiteDataSchema
+from swagger_server.models.site_data_schema_update import SiteDataSchemaUpdate
+from swagger_server.models.user_site_data import UserSiteData
+from swagger_server.models.user_site_data_update import UserSiteDataUpdate
