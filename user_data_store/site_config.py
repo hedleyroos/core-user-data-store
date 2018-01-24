3c05f698a0071076a8e2659981ae188428254b58
cobusc <cobus.carstens@gmail.com>
2018-01-24 11:12:12 +0200

    Refactoring of code. DB creation make target and script.

--- user_data_store/site_config.py
+++ user_data_store/site_config.py
@@ -4,5 +4,5 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-    "DB_URI", "postgresql+psycopg2://postgres@127.0.0.1:5432/user_data_store")
+        "DB_URI", "postgresql+psycopg2://user_data_store:password@127.0.0.1:5432/user_data_store")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
