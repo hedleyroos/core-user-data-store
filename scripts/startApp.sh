43b250926429d386503c134e7c62d9563b91867c
AltusBarry <AltusBarry@users.noreply.github.com>
2018-03-05 09:51:25 +0200

    develop: App container should not create db

--- scripts/startApp.sh
+++ scripts/startApp.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-sql/create_database.sh $(DB_NAME) $(DB_USER) | -u postgres psql -f -
 FLASK_APP=user_data_store/models.py flask db upgrade -d user_data_store/migrations
 python3 -m swagger_server
