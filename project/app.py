0be52782cfbd27abf31a858225b3b6399b7c9bc1
cobusc <cobus.carstens@gmail.com>
2018-04-17 09:27:48 +0200

    Removed irrelevant comment

--- project/app.py
+++ project/app.py
@@ -7,9 +7,6 @@ from project import settings
 APP = Flask(__name__)
 SENTRY = Sentry(dsn=settings.SENTRY_DSN)
 
-# Grab URI from env here, instead of getting it from settings. Original setting
-# implementation caused circular import issues for certain envs. Tox test env
-# for example.
 APP.config["SQLALCHEMY_DATABASE_URI"] = settings.SQLALCHEMY_DATABASE_URI
 APP.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = settings.SQLALCHEMY_TRACK_MODIFICATIONS
 
