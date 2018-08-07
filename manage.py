8d384435577386a9dabb57242bf4862d04a61563
cobusc <cobus.carstens@gmail.com>
2018-08-07 07:47:22 +0200

    Sentry is already loaded in project.app. Import models.

--- manage.py
+++ manage.py
@@ -1,21 +1,16 @@
 # All the imports and setup for migrations. flask_script allows for other
 # management commands to be run or integrated.
 if __name__ == "__main__":
-    import os
-    import logging
-
-    from raven.contrib.flask import Sentry
     from flask_migrate import Migrate, MigrateCommand
     from flask_script import Manager
 
     import project.app
-    # Do not import settings, as it requires the API config which we do not
-    # want to specify when running migrations.
+    # We _have_ to import models here. If not, the migrations will
+    # think that all tables have been dropped.
+    import user_data_store.models  # Do not remove
 
     APP = project.app.APP
     MIGRATE = Migrate(project.app.APP, project.app.DB)
-    SENTRY = Sentry(dsn=os.environ.get("SENTRY_DSN"))
-    SENTRY.init_app(APP, level=os.environ.get("SENTRY_LOG_LEVEL", logging.ERROR))
 
     manager = Manager(APP)
     manager.add_command("db", MigrateCommand)
